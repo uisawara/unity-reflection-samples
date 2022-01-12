bca9c29e35564b418bbd6914bc26d1ddd413949b
uisawara <yuichi.ogasawara.1121@gmail.com>
2022-01-12 10:08:14 +0900

    Add files via upload

---
+++ Runtime/TimeToBlackboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TimeToBlackboard : MonoBehaviour
+{
+    private void Update()
+    {
+        Blackboard.CurrentContext.Properties["Time.frameCount"] = Time.frameCount;
+        Blackboard.CurrentContext.Properties["Time.fixedTime"] = Time.fixedTime;
+        Blackboard.CurrentContext.Properties["Time.deltaTime"] = Time.deltaTime;
+        Blackboard.CurrentContext.Properties["Time.time"] = Time.time;
+    }
+}
